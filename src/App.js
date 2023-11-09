0f70deca6b922c858bbb219c73f6f0ba94e8405b
Kimheang <kimheang135@gmail.com>
2023-11-09 14:17:36 +0700

    feat: add btn clear items

--- src/App.js
+++ src/App.js
@@ -21,6 +21,13 @@ function App() {
     );
   }
 
+  function handleClearList() {
+    const confirmed = window.confirm(
+      'Are you sure you want to delete all items?'
+    );
+    if (confirmed) setItems([]);
+  }
+
   return (
     <div className="app">
       <Logo />
@@ -29,6 +36,7 @@ function App() {
         items={items}
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
+        onClearList={handleClearList}
       />
       <Stats items={items} />
     </div>
@@ -77,7 +85,7 @@ function Form({ onAddItems }) {
   );
 }
 
-function PackageList({ items, onDeleteItem, onToggleItem }) {
+function PackageList({ items, onDeleteItem, onToggleItem, onClearList }) {
   const [sortBy, setSortBy] = useState('input');
 
   let sortedItems;
@@ -111,6 +119,7 @@ function PackageList({ items, onDeleteItem, onToggleItem }) {
           <option value="description">Sort by description</option>
           <option value="packed">Sort by packed status</option>
         </select>
+        <button onClick={onClearList}>Clear list</button>
       </div>
     </div>
   );
